4a0fa8f2e7f5483dbd13700f5e26ba97ca633476
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-02-28 12:02:00 -0500

    Enabling scheduling to run service everyday.

--- src/main/java/com/javaworks/coronavirusstats/CoronavirusStatsApplication.java
+++ src/main/java/com/javaworks/coronavirusstats/CoronavirusStatsApplication.java
@@ -2,8 +2,10 @@ package com.javaworks.coronavirusstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusStatsApplication {
 
 	public static void main(String[] args) {

6e6b85b7c39a9ab7f77f97a3645c33ffa5a3955c
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-02-28 11:15:57 -0500

    Initial project setup(Spring Initializr)

---
+++ src/main/java/com/javaworks/coronavirusstats/CoronavirusStatsApplication.java
@@ -0,0 +1,13 @@
+package com.javaworks.coronavirusstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaworks/coronavirusstats/CoronavirusStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaworks.coronavirusstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
